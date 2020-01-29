666c1d52e84db6d41e09c28d7ef9e9937ffdf843
Raj Thakkar <thakkarr95@gmail.com>
2020-01-29 01:44:03 -0500

    Adding the emailApp.java file

--- src/emailapp/Main.java
+++
@@ -1,8 +0,0 @@
-package emailapp;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/emailapp/emailApp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class emailApp {
+
+    public static void main(String[] args) {
+	// write your code here
+        email em1 = new email("John", "Smith");
+
+        System.out.println(em1.showInfo());
+    }
+}
