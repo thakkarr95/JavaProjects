f2850bb161e5fa534a3af1ea2c762e54d54e3801
Raj Thakkar <thakkarr95@gmail.com>
2020-01-29 03:41:22 -0500

    Adding the Student Database Application with array of objects of class Student

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,17 +1,31 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
 
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.showInfo());
 
         // Ask how many students we want to add
 
+        System.out.print(" Enter the number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+
+        // This how we can create an array of objects of class Student
+        Student[] students = new Student[numOfStudents];
+
         // Create n number of new students
+
+        for (int n = 0; n < numOfStudents; n++) {
+
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            System.out.println(students[n].showInfo());
+
+        }
 
     }
 }
